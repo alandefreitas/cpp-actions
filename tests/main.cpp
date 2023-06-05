c0907ef179630d336180bafaaa8024dfd71a488d
alandefreitas <alandefreitas@gmail.com>
2023-06-05 20:48:41 -0300

    ci: test No-Deps factor

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,16 +1,23 @@
-#include <boost/variant2.hpp>
-#include <fmt/format.h>
+#ifndef CPP_ACTIONS_NO_DEPS
+#    include <boost/variant2.hpp>
+#    include <fmt/format.h>
+#endif
 #include <cassert>
+#include <string>
 
 int
 main() {
-    boost::variant2::variant<int, bool> v(2);
     std::string s;
+#ifndef CPP_ACTIONS_NO_DEPS
+    boost::variant2::variant<int, bool> v(2);
     if (v.index() == 0) {
         s = fmt::format("Hello, int!\n");
     } else {
         s = fmt::format("Hello, bool!\n");
     }
+#else
+    s = "Hello, int!\n";
+#endif
     assert(s == "Hello, int!\n");
     return 0;
 }
