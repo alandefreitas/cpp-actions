b63409571441a1cca4e3a714a07c06ec8c670555
alandefreitas <alandefreitas@gmail.com>
2023-06-07 04:36:56 -0300

    feat(flamegraph): report path option

--- flamegraph/combine-traces.py
+++ flamegraph/combine-traces.py
@@ -15,7 +15,6 @@ Adapted from: https://www.snsystems.com/technology/tech-blog/clang-time-trace-fe
 
 import argparse
 import json
-import sys
 import os
 import re
 
@@ -37,6 +36,7 @@ def find_trace_files(directory):
                 trace_files.append(os.path.join(root, file))
     return trace_files
 
+
 def find_compile_commands(directory):
     current_dir = os.path.abspath(directory)
 
@@ -50,6 +50,7 @@ def find_compile_commands(directory):
 
     return None
 
+
 def load_compile_commands(directory):
     path = find_compile_commands(directory)
     if path is None:
@@ -57,10 +58,12 @@ def load_compile_commands(directory):
     with open(path, 'r') as f:
         return json.load(f)
 
+
 def is_subpath(child_path, parent_path):
     child_path = os.path.abspath(child_path)
     parent_path = os.path.abspath(parent_path)
     return child_path.startswith(parent_path + '/')
+
 
 def extract_include_paths(command):
     include_paths = []
@@ -82,13 +85,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Installs the dependencies needed to test a Boost library.')
     parser.add_argument('--source-dir', help="directory to scan", default=os.getcwd())
     parser.add_argument('--build-dir', help="directory to scan", default=os.getcwd())
-    parser.add_argument('-o', '--output', help="output file", default='combined-traces.md')
+    parser.add_argument('-o', '--output', help="output file", default='combined-traces.json')
+    parser.add_argument('--report-output', help="output file", default='time-trace-report.md')
     parser.add_argument('--verbose', action='store_true', help="Verbose mode")
     args = parser.parse_args()
 
     source_dir = os.path.abspath(args.source_dir)
     build_dir = os.path.abspath(args.build_dir)
-    output = os.path.join(build_dir, args.output)
+    output_path = os.path.join(build_dir, args.output)
+    report_path = os.path.join(build_dir, args.report_output)
     verbose = args.verbose
 
     # Find trace files
@@ -117,6 +122,22 @@ if __name__ == '__main__':
     include_paths.append('/usr/local/include')
     include_paths.append('/usr/include/c++')
 
+    # Report data
+    total_compile: {int} = (0, 0)
+    # Frontend: parsing and instantiations
+    total_frontend: {int} = (0, 0)
+    total_parsing: {int} = (0, 0)
+    total_instantiations: {int} = (0, 0)
+    # Backend: code generation and optimizations
+    total_backend: {int} = (0, 0)
+    total_codegen: {int} = (0, 0)
+    total_optimize: {int} = (0, 0)
+    # Individual files and symbols
+    file_compile: {str, tuple} = {}
+    file_parse: {str, tuple} = {}
+    symbol_parse: {str, tuple} = {}
+    symbol_instantiate: {str, tuple} = {}
+
     # Combine time-trace content
     start_time = 0
     combined_data = []
@@ -137,14 +158,18 @@ if __name__ == '__main__':
         if filename.find('../') != -1:
             filename = os.path.abspath(filename)
 
-        for event in trace['traceEvents']:
+        # regions already accounted for
+        parsing_regions = []
+        instantiation_regions = []
+
+        sorted_events = sorted(trace['traceEvents'], key=lambda x: 0 if 'dur' not in x else int(x['dur']), reverse=True)
+        for event in sorted_events:
             # Filter out very short events to reduce data size
-            event_is_too_short = event['ph'] == 'M' or event['name'].startswith('Total') or event['dur'] < 5000
+            event_is_too_short = event['ph'] == 'M' or event['name'].startswith('Total')
             if event_is_too_short:
                 continue
 
-            log('Event:', event)
-
+            # log('Event:', event)
             # Adjust detail path
             if 'args' in event and 'detail' in event['args']:
                 if type(event['args']['detail']) == str:
@@ -161,6 +186,78 @@ if __name__ == '__main__':
                                     break
                         log(f"Detail Path: {event['args']['detail']}")
 
+            # Store data for the report
+            if event['name'] == 'Source':
+                # add to total
+                ts = int(event['ts'])
+                dur = int(event['dur'])
+                accounted_for = False
+                for parsing_region in parsing_regions:
+                    if parsing_region[0] <= ts < parsing_region[1]:
+                        accounted_for = True
+                        break
+                if not accounted_for:
+                    total_parsing = (total_parsing[0] + 1, total_parsing[1] + dur)
+                    parsing_regions.append((ts, ts + dur))
+
+                # add to files total
+                file = event['args']['detail']
+                if file not in file_parse:
+                    file_parse[file] = (0, 0)
+                file_parse[file] = (file_parse[file][0] + 1, file_parse[file][1] + dur)
+
+            elif event['name'].startswith('Parse') and 'args' in event and 'detail' in event['args']:
+                # add to symbols total
+                dur = int(event['dur'])
+                symbol = event['args']['detail']
+                if symbol not in symbol_parse:
+                    symbol_parse[symbol] = (0, 0)
+                symbol_parse[symbol] = (symbol_parse[symbol][0] + 1, symbol_parse[symbol][1] + dur)
+
+            elif event['name'].startswith('Instantiate') and 'args' in event and 'detail' in event['args']:
+                # add to total
+                ts = int(event['ts'])
+                dur = int(event['dur'])
+                accounted_for = False
+                for instantiation_region in instantiation_regions:
+                    if instantiation_region[0] <= ts < instantiation_region[1]:
+                        accounted_for = True
+                        break
+                if not accounted_for:
+                    total_instantiations = (total_instantiations[0] + 1, total_instantiations[1] + dur)
+                    instantiation_regions.append((ts, ts + dur))
+
+                # add to symbol total
+                symbol = event['args']['detail']
+                if symbol not in symbol_instantiate:
+                    symbol_instantiate[symbol] = (0, 0)
+                symbol_instantiate[symbol] = (symbol_instantiate[symbol][0] + 1, symbol_instantiate[symbol][1] + dur)
+
+            elif event['name'] == 'PerformPendingInstantiations':
+                ts = int(event['ts'])
+                dur = int(event['dur'])
+                total_instantiations = (total_instantiations[0] + 1, total_instantiations[1] + dur)
+                instantiation_regions.append((ts, ts + dur))
+
+            elif event['name'] == 'Frontend':
+                total_frontend = (total_frontend[0] + 1, total_frontend[1] + int(event['dur']))
+            elif event['name'] == 'Backend':
+                total_backend = (total_backend[0] + 1, total_backend[1] + int(event['dur']))
+            elif event['name'] == 'Optimizer':
+                total_optimize = (total_optimize[0] + 1, total_optimize[1] + int(event['dur']))
+            elif event['name'] == 'CodeGenPasses':
+                total_codegen = (total_codegen[0] + 1, total_codegen[1] + int(event['dur']))
+            elif event['name'] == 'ExecuteCompiler':
+                # add to total
+                ts = int(event['ts'])
+                dur = int(event['dur'])
+                total_compile = (total_compile[0] + 1, total_compile[1] + dur)
+
+                # add to files total
+                if filename not in file_compile:
+                    file_compile[filename] = (0, 0)
+                file_compile[filename] = (file_compile[filename][0] + 1, file_compile[filename][1] + dur)
+
             # Keep track of the main ExecuteCompiler event, which exists for each file
             # Also adapt this event to include the file name
             if event['name'] == 'ExecuteCompiler':
@@ -172,7 +269,6 @@ if __name__ == '__main__':
                 # Set the file name in ExecuteCompiler
                 if 'args' not in event:
                     event['args'] = {}
-
                 event['args']['detail'] = filename
 
             # Replace source event names with filename
@@ -199,6 +295,124 @@ if __name__ == '__main__':
         # Add 1 to avoid issues with simultaneous events
         start_time += file_total_time + 1
 
-    with open(output, 'w') as f:
+    with open(output_path, 'w') as f:
         json.dump({'traceEvents': sorted(combined_data, key=lambda k: k['ts'])}, f)
-        log('Saved to ', os.path.abspath(output))
+        log('Saved to ', os.path.abspath(output_path))
+
+
+    # Report
+    def format_time(microseconds):
+        if microseconds < 1000:
+            return f"{round(microseconds, 2)} µs"
+        elif microseconds < 1000000:
+            milliseconds = round(microseconds / 1000, 2)
+            return f"{milliseconds} ms"
+        else:
+            seconds = round(microseconds / 1000000, 2)
+            return f"{seconds} s"
+
+
+    # Report output
+    output = '# Time Trace\n\n'
+    output += '## Summary\n\n'
+    output += '| Step | %     | Total Time | Avg. | Count |\n'
+    output += '| --------- | ----- | ---------- | ------------ | ----- |\n'
+    if total_compile[0] != 0:
+        output += f'| Compile   | 100%   | {format_time(total_compile[1])} | {format_time(total_compile[1] / total_compile[0])} | {total_compile[0]} |\n'
+    if total_frontend[0] != 0:
+        output += f'| 1) Frontend   | {round(100 * total_frontend[1] / total_compile[1], 2)}% | {format_time(total_frontend[1])} | {format_time(total_frontend[1] / total_frontend[0])} | {total_frontend[0]} |\n'
+    if total_parsing[0] != 0:
+        output += f'| 1A) Parsing   | {round(100 * total_parsing[1] / total_compile[1], 2)}% | {format_time(total_parsing[1])} | {format_time(total_parsing[1] / total_parsing[0])} | {total_parsing[0]} |\n'
+    if total_instantiations[0] != 0:
+        output += f'| 1B) Instantiations   | {round(100 * total_instantiations[1] / total_compile[1], 2)}% | {format_time(total_instantiations[1])} | {format_time(total_instantiations[1] / total_instantiations[0])} | {total_instantiations[0]} |\n'
+    if total_backend[0] != 0:
+        output += f'| 2) Backend   | {round(100 * total_backend[1] / total_compile[1], 2)}% | {format_time(total_backend[1])} | {format_time(total_backend[1] / total_backend[0])} | {total_backend[0]} |\n'
+    if total_codegen[0] != 0:
+        output += f'| 2A) Code Generation   | {round(100 * total_codegen[1] / total_compile[1], 2)}% | {format_time(total_codegen[1])} | {format_time(total_codegen[1] / total_codegen[0])} | {total_codegen[0]} |\n'
+    if total_optimize[0] != 0:
+        output += f'| 2B) Optimization   | {round(100 * total_optimize[1] / total_compile[1], 2)}% | {format_time(total_optimize[1])} | {format_time(total_optimize[1] / total_optimize[0])} | {total_optimize[0]} |\n'
+    output += '\n\n'
+
+    output += '## Files\n\n'
+
+
+    def section_table(column_name, data):
+        section_output = f'| {column_name} | %    | Total Time | Avg. | Count |\n'
+        section_output += '| --------- | ---------- | ---------- | ------------ | ----- |\n'
+
+        data = dict(sorted(data.items(), key=lambda x: x[1][1], reverse=True))
+        acc = 0
+        for [_, v] in data.items():
+            [_, time] = v
+            acc += time
+
+        n = 0
+        for [file, v] in data.items():
+            [count, time] = v
+            section_output += f'| `{file}` | {round(100 * time / acc, 2)} % | {format_time(time)} | {format_time(time / count)} | {count} |\n'
+            n += 1
+            if n > 7:
+                break
+        section_output += '\n\n'
+
+        if n > 7:
+            section_output += '<details>\n<summary>More...</summary>\n\n'
+            section_output += f'| {column_name} | %    | Total Time | Avg. | Count |\n'
+            section_output += '| --------- | ---------- | ---------- | ------------ | ----- |\n'
+            n = 0
+            for [file, v] in data.items():
+                [count, time] = v
+                section_output += f'| `{file}` | {round(100 * time / acc, 2)} % | {format_time(time)} | {format_time(time / count)} | {count} |\n'
+                n += 1
+                if n > 100:
+                    break
+            section_output += '\n\n'
+            section_output += '</details>\n\n'
+        return section_output
+
+
+    output += '### Compile\n\n'
+    output += section_table('File', file_compile)
+    output += '### Parse\n\n'
+    output += section_table('File', file_parse)
+
+    output += '## Symbols\n\n'
+
+    output += '### Parse\n\n'
+    output += section_table('Symbol', symbol_parse)
+    output += '### Instantiate\n\n'
+    output += section_table('Symbol', symbol_instantiate)
+    output += '### Instantiate Sets\n\n'
+
+
+    def convert_template_string(input_string):
+        l = 0
+        output_str = ''
+        for c in input_string:
+            if l == 0:
+                output_str += c
+            if c == '<':
+                if l == 0:
+                    output_str += '$'
+                l += 1
+            elif c == '>':
+                l -= 1
+                if l == 0:
+                    output_str += c
+        return output_str
+
+
+    symbol_set_instantiate: {str, tuple} = {}
+    for [symbol, v] in symbol_instantiate.items():
+        [count, time] = v
+        symbol_set = convert_template_string(symbol)
+        if symbol_set not in symbol_set_instantiate:
+            symbol_set_instantiate[symbol_set] = (0, 0)
+        symbol_set_instantiate[symbol_set] = (
+            symbol_set_instantiate[symbol_set][0] + count, symbol_set_instantiate[symbol_set][1] + time)
+    output += section_table('Symbol Set', symbol_set_instantiate)
+
+    log(output)
+    with open(report_path, 'w') as f:
+        f.write(output)
+        log('Report saved to ', os.path.abspath(report_path))
