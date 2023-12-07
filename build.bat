a9d5d09b496854b56135c6cfeaf9392618fe9087
alandefreitas <alandefreitas@gmail.com>
2023-12-07 19:00:53 -0300

    build: add build.bat script

---
+++ build.bat
@@ -0,0 +1,28 @@
+REM Description: Build all the javascript projects in the repository
+
+cd setup-program
+call npm install
+call npm run all
+cd ..
+
+cd setup-gcc
+call npm install
+call npm run all
+cd ..
+
+cd setup-clang
+call npm install
+call npm run all
+cd ..
+
+cd setup-cpp
+call npm install
+call npm run all
+cd ..
+
+cd cpp-matrix
+call npm install
+call npm run all
+cd ..
+
+call python3 docs/parse_actions.py
