a1e991ecca7c7f1d79ca0feab84d669b56212f7f
Alan de Freitas <alandefreitas@gmail.com>
2025-11-03 16:20:39 -0500

    build: new update-dependencies.sh script

---
+++ update-dependencies.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Description: Update dependencies across all JavaScript actions
+set -euo pipefail
+
+projects_with_package=()
+
+for dir in */; do
+    if [ "$dir" == "docs/" ]; then
+        continue
+    fi
+
+    if [ -f "${dir}package.json" ]; then
+        projects_with_package+=("$dir")
+    fi
+done
+
+while IFS= read -r common_dir; do
+    projects_with_package+=("$common_dir")
+done < <(find common -mindepth 1 -maxdepth 1 -type d -exec test -f "{}/package.json" \; -printf '%p/\n')
+
+for project in "${projects_with_package[@]}"; do
+    (
+        cd "$project"
+        echo "==== Updating dependencies for $project ===="
+        npx npm-check-updates -u
+        npm install
+    )
+done
